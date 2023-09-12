428a94701188a9ab260a100f0d5ca00f429d8bfe
KKaviya1 <Kaviyagandhi136@gmail.com>
2023-09-12 16:17:12 +0530

    my commmit

---
+++ App/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Let us Display List of Month");
+List<string> listMonth=new List<string>
+{
+    "Jan","Feb","Mar","April","May","June","July","Aug","Sep","Oct","Nov","Dec"
+};
+
+foreach(var month in listMonth)
+{
+    Console.WriteLine(month);
+    Thread.Sleep(2000);
+}
---
+++ WelcomeApp/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome to counting..");
+
+for(int i=1;i<=30;i++)
+{
+    Console.Write(i+"\t");
+    Thread.Sleep(1000);
+}
